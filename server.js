444cc8955bec46fff5ae568ad04450ba12936e1d
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-03-24 21:10:39 +0100

    Modularise the server.js using json answers
    Slack supports json answers with richer formatting, exploit this feature
    and at the same time modularize the code by separating the server and
    the messages it sends back.
    
    Supported json can be generated using:
    https://api.slack.com/tools/block-kit-builder?mode=message&blocks=%5B%5D

--- server.js
+++ server.js
@@ -6,34 +6,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-  res.end('Welcome to the slackbot "community" for [data against covid-19](https://app.slack.com/client/TUQTGE7FU)')
+  res.send('Welcome to the slackbot "community" for [data against covid-19](https://app.slack.com/client/TUQTGE7FU)')
 });
 
 app.post('/',function(req,res){
 
   let answer = ""
   let commands = require('./commands.json');
-  let aide = "Ce service vous aidera à orienter correctement votre action sur ce slack.\nVous pouvez poser certaines questions à ce robot et ce dernier vous aidera à obtenir la bonne réponse.\nPour obtenir la liste des questions que vous pouvez poser, utilisez la commande 'aide' de cette manière : \n```/community aide```"
+  //"Ce service vous aidera à orienter correctement votre action sur ce slack.\nVous pouvez poser certaines questions à ce robot et ce dernier vous aidera à obtenir la bonne réponse.\nPour obtenir la liste des questions que vous pouvez poser, utilisez la commande 'aide' de cette manière : \n```/community aide```"
 
   if (req.body.text == "") {
-      answer = aide
+      answer = require('./messages/_.json');
   } else if (req.body.text.toLowerCase() == "aide") {
-      answer = "*Bienvenue sur Community*, le robot d'aide à l'action du slack *Data Against Covid-19*\n"
-      answer += "Merci d'être ici, nous avons besoin de vous!\n\n"
-      answer += "Pour poser une question à ce robot, utilisez la syntax suivante : ```/community [Ma Question]```\n"
-      answer += "*Questions que vous pourriez posez :*\n"
+      answer = require('./messages/aide.json');
       for (let key in commands) {
-        answer += '- ' + key + '\n'
+        answer["blocks"][1]["text"]["text"] += '- ' + key + '\n'
       }
-      answer += "\nPar exemple : ```/community je veux proposer mon aide```\n"
-      answer += "*La liste des questions va s'accroitre de manière régulière jusqu'à pouvoir vous aider dans tous les aspects de votre participation.*"
   } else if (commands[req.body.text.toLowerCase()]) {
       answer = commands[req.body.text.toLowerCase()]
   } else {
-      answer = 'Commande inconnue. Utilisez la commande ```/community aide``` pour plus d\'information.'
+      answer = require('./messages/unknown_command_error.json');
   }
 
-  res.end(answer)
+  res.send(answer)
 
 });
 
