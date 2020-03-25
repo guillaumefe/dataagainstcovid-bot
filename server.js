6cb3b0c212c4a30762467e22c48d677813a7d17f
Guillaume Fe <guillaume.ferron@gmail.com>
2020-03-25 08:53:13 +0100

    Correct issue#5
    Update answer instead of answer["blocks"][1]["text"]["text"] (that may be cached somewhere by heroku, that makes "aide" messages concatenating)

--- server.js
+++ server.js
@@ -15,13 +15,18 @@ app.post('/',function(req,res){
   let commands = require('./commands.json');
   //"Ce service vous aidera à orienter correctement votre action sur ce slack.\nVous pouvez poser certaines questions à ce robot et ce dernier vous aidera à obtenir la bonne réponse.\nPour obtenir la liste des questions que vous pouvez poser, utilisez la commande 'aide' de cette manière : \n```/community aide```"
 
+  
+  //TODO
+  // Adapt code to search messages/ dir and not in commands object
   if (req.body.text == "") {
       answer = require('./messages/_.json');
   } else if (req.body.text.toLowerCase() == "aide") {
       answer = require('./messages/aide.json');
+      answer += answer["blocks"][1]["text"]["text"]
       for (let key in commands) {
-        answer["blocks"][1]["text"]["text"] += '- ' + key + '\n'
+        answer += '- ' + key + '\n'
       }
+      answer += answer["blocks"][3]["text"]["text"]
   } else if (commands[req.body.text.toLowerCase()]) {
       answer = commands[req.body.text.toLowerCase()]
   } else {
