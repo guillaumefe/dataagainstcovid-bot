7ded980b3a295669daf5b00b850bad0b72c86006
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-03-25 14:50:12 +0100

    Ne pas repeter la comprehension des commandes
    Generer une liste de commande valable seulement au debut d'une session
    et ne pas repeter l'operation.
    
    Devrait reduire le temps de latence.

--- server.js
+++ server.js
@@ -10,42 +10,42 @@ app.get('/',function(req,res){
   res.send('Welcome to the slackbot "community" for [data against covid-19](https://app.slack.com/client/TUQTGE7FU)')
 });
 
+// Prepare list of available commands
+// Load old format commands from commands.json
+let commands = require('./commands.json');
+let command_map = {};
+
+function add_to_command_map (map, key, phrase) {
+  // Function which adds command aliases to an alias -> command map
+  map[phrase.toLowerCase()] = key.toLowerCase();
+  map[phrase] = key.toLowerCase();
+}
+
+for (let key in commands) {
+  add_to_command_map(command_map, key, key);
+}
+// Collect valid commands from commands.json and messages/commands/ folder 
+let command_files_dir = './messages/commands/';
+let command_files = fs.readdirSync(command_files_dir);
+
+command_files.forEach(function (item, index) {
+  command_file = require(command_files_dir + "/" + item);
+  let new_key = ""
+  try {
+    new_key = command_file["command"][0];
+    command_file["command"].forEach(function (phrase, _phrase_index){
+      add_to_command_map(command_map, new_key, phrase);
+    });
+  } catch (err) {
+    new_key = command_file["command"];
+    add_to_command_map(command_map, new_key, new_key);
+  }
+  commands[new_key.toLowerCase()] = command_file["answer"];
+});
+console.log(command_map);
+
 app.post('/',function(req,res){
-
   let answer = ""
-  // Load old format commands from commands.json
-  let commands = require('./commands.json');
-  let command_map = {};
-
-  function add_to_command_map (map, key, phrase) {
-    // Function which adds command aliases to an alias -> command map
-    map[phrase.toLowerCase()] = key.toLowerCase();
-    map[phrase] = key.toLowerCase();
-  }
-
-  for (let key in commands) {
-    add_to_command_map(command_map, key, key);
-  }
-  // Collect valid commands from commands.json and messages/commands/ folder 
-  let command_files_dir = './messages/commands/';
-  let command_files = fs.readdirSync(command_files_dir);
-
-  command_files.forEach(function (item, index) {
-    command_file = require(command_files_dir + "/" + item);
-    let new_key = ""
-    try {
-      new_key = command_file["command"][0];
-      command_file["command"].forEach(function (phrase, _phrase_index){
-        add_to_command_map(command_map, new_key, phrase);
-      });
-    } catch (err) {
-      new_key = command_file["command"];
-      add_to_command_map(command_map, new_key, new_key);
-    }
-    commands[new_key.toLowerCase()] = command_file["answer"];
-  });
-  console.log(command_map);
-
   //TODO
   // Adapt code to search messages/ dir and not in commands object
   if (req.body.text == "") {
