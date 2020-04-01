0b073308813711535f1082d169eedf738a1931c7
Guillaume Ferron <guillaume.ferron@gmail.com>
2020-04-01 02:16:31 +0200

    package command infrastructure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const request = require('request');
 const favicon = require('serve-favicon')
 const path = require('path')
@@ -8,102 +7,19 @@ const crypto = require('crypto');
 
 const app = express();
 
-// TODO paackage the command infrastructure into a module or whatever JS 
-// calls them
-const command_map = {
-  "alias_to_cmd": {},
-  "cmd_to_alias": {},
-  // TODO: make command_map an object
-  add_key: function(key, phrase) {
-    // Function which adds command aliases to an alias -> command map
-    lower_case_key = key.toLowerCase();
-    this["alias_to_cmd"][phrase.toLowerCase()] = lower_case_key;
-    this["alias_to_cmd"][phrase] = lower_case_key;
-    if(lower_case_key in this["cmd_to_alias"]){
-      this["cmd_to_alias"][lower_case_key].push(phrase);
-    } else {
-      this["cmd_to_alias"][lower_case_key] = [phrase];
-    }
-  }
-};
+// TODO package the command infrastructure into a module
+const command_infrastructure = require('./commands');
+const command_map = command_infrastructure.command_map
+const commandsFromFolder = command_infrastructure.commandsFromFolder
+const generateCommandHelp = command_infrastructure.generateCommandHelp
+const respondToCommand = command_infrastructure.respondToCommand
+const cmd_list_prefix = command_infrastructure.cmd_list_prefix
+const cmd_list_alias_pre = command_infrastructure.cmd_list_alias_pre
+const cmd_list_alias_sep = command_infrastructure.cmd_list_alias_sep
+const cmd_list_alias_post = command_infrastructure.cmd_list_alias_post
+const commands =  command_infrastructure.commands
 
-function commandsFromFolder(command_directory, commands, command_map) {
-  // Explores all json files in a directory and looks for files matching the 
-  // command format of objects with fields "command" and "answer"
-
-  const command_files = fs.readdirSync(command_directory);
-  for(i=0; i<command_files.length; i++) {
-    item = command_files[i];
-    // Only act on JSON files otherwise continue (skip rest of loop)
-    if(item.slice(-5).toLowerCase() != ".json"){
-      console.log(item + " is not a .json file, skipping");
-      continue;
-    }
-    
-    command_file = require(command_directory + "/" + item);
-    if (Array.isArray(command_file)){
-      command_file.forEach(function (command_item, _){
-        parseAndMapCommand(command_item, commands, command_map);
-      });
-    } else {
-      parseAndMapCommand(command_file, commands, command_map)
-    }
-  }
-}
-
-function parseAndMapCommand(command_file, commands, command_map){
-  // Only act on valid command JSON files as defined by needing a 
-    // "command" and an "answer" field
-    const check_properties = ("command" in command_file) && ("answer" in command_file);
-    if(!check_properties){
-      console.log(item + "does not contain a valid command object, make sure fields"
-        + "'command'(Array|String) and 'answer' are defined");
-      return;
-    }
-
-    // Now we have a valid command file lets process it
-    // Commands can have aliases, e.g. a shorter name to type
-    let new_key = ""
-    try { // assume there are multiple names for that command
-      new_key = command_file["command"][0];  
-      command_file["command"].forEach(function (phrase, _phrase_index){
-        command_map.add_key(new_key, phrase);
-      });
-    } catch (err) { // Catch error from array access if there is a single name for the command
-      new_key = command_file["command"];
-      command_map.add_key(new_key, new_key);
-    }
-    // Match the answer to the principal command name (n.b. the first to appear)
-    commands[new_key.toLowerCase()] = command_file["answer"];
-}
-
-const cmd_list_prefix = ' :arrow_right:  ';
-const cmd_list_alias_pre = ' (aka: ';
-const cmd_list_alias_sep = ';';
-const cmd_list_alias_post = ')';
-function generateCommandHelp(commands, command_map) {
-  // Generates markdown text with the name of the commands 
-  // and their known aliases.
-  let command_list = "";
-  for (const key in commands) {
-    command_list += cmd_list_prefix + key;
-    for (let i=1; i<command_map["cmd_to_alias"][key].length; i++){
-      if (i==1){
-        command_list += cmd_list_alias_pre;
-      }
-      command_list += command_map["cmd_to_alias"][key][i]
-      if (i==command_map["cmd_to_alias"][key].length-1){
-        command_list += cmd_list_alias_post;
-      } else {
-        command_list += cmd_list_alias_sep;
-      }
-    }
-    command_list += "\n";
-  }
-  return command_list;
-}
-
-function PrepareSpecialAnswers(command_lists, commands, command_map) {
+const PrepareSpecialAnswers = function(command_lists, commands, command_map) {
   // Prepares answers that need additional combining 
   // aide; documentation and doc-dec all parse a list of commands into the
   // message
@@ -135,6 +51,7 @@ function PrepareSpecialAnswers(command_lists, commands, command_map) {
     commands[command_map.alias_to_cmd[incoming_cmd.toLowerCase()]] = answer;
   }
 }
+
 // ============================
 /// Preprocessing
 // ============================
@@ -147,10 +64,8 @@ app.get('/',function(req,res){
   res.send('Welcome to the slackbot "community" for [data against covid-19](https://app.slack.com/client/TUQTGE7FU)')
 });
 
-
 // Collect valid commands from commands.json and messages/commands/ folder 
 // and messages/commands_hidden
-const commands = {}; // simple text commands
 // Parse commands in folders
 const command_lists = {};
 commandsFromFolder('./messages/commands_prioritaires/', commands, command_map);
@@ -169,19 +84,6 @@ console.log(command_lists);
 /// Event handling
 // ============================
 
-
-function respondToCommand(incoming_cmd) {
-  // Respond to a command that was received.
-  let answer = ""
-  if (incoming_cmd == "") {
-      answer = require('./messages/_.json');
-  } else if (command_map["alias_to_cmd"][incoming_cmd.toLowerCase()]) {
-      answer = commands[command_map["alias_to_cmd"][incoming_cmd.toLowerCase()]];
-  } else {
-      answer = require('./messages/unknown_command_error.json');
-  }
-  return answer;
-}
 function privateLogLine(trigger_id, channel, user_name, payload_type, 
   action, request_type){
   console.log(user_name);
