9db5bddb93c03fe087149238a4b6f6bebcd918f2
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-01 20:18:51 +0200

    Move server to fix deployment

--- server.js
+++ server.js
@@ -5,21 +5,20 @@ const path = require('path')
 
 const app = express()
 
-const command_infrastructure = require('./commands')
+const command_infrastructure = require(path.join(__dirname, 'core', 'commands'))
 const command_map = command_infrastructure.command_map
 const commandsFromFolder = command_infrastructure.commandsFromFolder
 const generateCommandHelp = command_infrastructure.generateCommandHelp
 const commands =  command_infrastructure.commands
 
-const answer_infrastructure = require('./answers')
+const answer_infrastructure = require(path.join(__dirname, 'core', 'answers'))
 const PrepareSpecialAnswers = answer_infrastructure.PrepareSpecialAnswers
-const privateLogLine = answer_infrastructure.privateLogLine
 
 // ============================
 /// Preprocessing
 // ============================
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'core','public', 'favicon.ico')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -27,11 +26,14 @@ app.use(bodyParser.json())
 // and messages/commands_hidden
 // Parse commands in folders
 const command_lists = {}
-commandsFromFolder('./messages/commands_prioritaires/', commands, command_map)
+const cmd_folder_priority = path.join(__dirname, 'core', 'messages', 'commands_prioritaires')
+const cmd_folder = path.join(__dirname, 'core', 'messages', 'commands')
+const cmd_folder_hidden = path.join(__dirname, 'core', 'messages', 'commands_hidden')
+commandsFromFolder(cmd_folder_priority, commands, command_map)
 command_lists["aide"] = generateCommandHelp(commands, command_map)
-commandsFromFolder('./messages/commands/', commands, command_map)
+commandsFromFolder(cmd_folder, commands, command_map)
 command_lists["documentation"] = generateCommandHelp(commands, command_map)
-commandsFromFolder('./messages/commands_hidden/', commands, command_map)
+commandsFromFolder(cmd_folder_hidden, commands, command_map)
 command_lists["doc-dev"] = generateCommandHelp(commands, command_map)
 
 PrepareSpecialAnswers(command_lists, commands, command_map)
@@ -43,7 +45,7 @@ console.log(command_lists)
 /// Event handling
 // ============================
 
-require('./router')(app)
+require(path.join(__dirname, 'core', 'router'))(app)
 
 port = process.env.PORT || 8000
 app.listen(port,function(){
